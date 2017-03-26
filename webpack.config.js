383905dbeb41bb37a7d38b5c547fb9cbe9c25baf
Vodka <c10854@aliyun.com>
2017-03-26 14:01:04 +0800

    home-2017.3.26

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtracTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = function (env) {
+    return {
+        // 入口文件
+        entry: {
+            main: './src/index.js',
+            vendor: 'moment'
+        },
+        // 输出位置
+        output: {
+            filename: '[name].[chunkhash].js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        resolve: {
+            // 设置什么目录会被搜索
+            modules: [
+                "node_modules"
+            ],
+            extensions: [".js"],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ExtracTextPlugin.extract({
+                        use: 'css-loader'
+                    })
+                },
+                {
+                    test: /\.jsx$/,
+                    loader: "babel-loader",
+                }
+            ]
+        },
+        plugins: [
+            // 独立打包
+            new ExtracTextPlugin('styles.css'),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: ['vender', 'manifest']
+            }),
+            new HtmlWebpackPlugin({
+                template: './index.html'
+            })
+        ]
+    }
+}
